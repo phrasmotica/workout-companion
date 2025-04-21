879623c8dc0a295e7fb8aed04d89a5797b8c3da3
Julian Lawrance <julian.lawrance@gmail.com>
2025-04-21 17:47:24 +0100

    Create scene and script for providing workout data to the entire app

---
+++ scripts/workout_provider.gd
@@ -0,0 +1,40 @@
+@tool
+class_name WorkoutProvider extends Node
+
+@export
+var workout: Workout:
+    set(value):
+        workout = value
+
+        if not workout.changed.is_connected(_emit_changed):
+            workout.changed.connect(_emit_changed)
+
+        workout_changed.emit(workout)
+
+signal workout_changed(workout: Workout)
+
+func _emit_changed() -> void:
+    workout_changed.emit(workout)
+
+func get_phase(index: int) -> WorkoutPhase:
+    if not workout:
+        return null
+
+    if index < 0 or index >= workout.phases.size():
+        return null
+
+    return workout.phases[index]
+
+func get_current_phase() -> WorkoutPhase:
+    return workout.get_current_phase() if workout else null
+
+func get_phase_count() -> int:
+    return workout.phases.size() if workout else 0
+
+func get_step_count() -> int:
+    if not workout:
+        return 0
+
+    return workout.phases \
+        .map(func(p: WorkoutPhase): return p.sets) \
+        .reduce(func(accum: int, current: int): return accum + current)

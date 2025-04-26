9629fc30b423a92f3c8c4442ace7ef1213e06dbf
Julian Lawrance <julian.lawrance@gmail.com>
2025-04-26 18:23:33 +0100

    Create dedicated script for updating the UI

--- scripts/app.gd
+++ scripts/app.gd
@@ -15,26 +15,18 @@ var workout_provider: WorkoutProvider:
 		_refresh()
 
 @export
-var status_message: StatusMessage
+var ui_updater: UIUpdater
 
-@export
-var phase_counter: PhaseCounter
-
-@export
-var rep_counter: RepCounter
-
-@onready
-var countdown: Countdown = %Countdown
-
-@onready
-var pause_countdown: Countdown = %PauseCountdown
-
-@onready
-var flasher: Flasher = %Flasher
+# HIGH: move all of the state-machine transitions into a dedicated script that
+# emits signals when the transitions happen. Then react to those signals in
+# this script.
 
 var _current_phase := -1
 var _sets_remaining := 0
 var _reps_remaining := 0
+
+func _ready() -> void:
+	assert(ui_updater)
 
 func _handle_workout_changed(_workout: Workout) -> void:
 	_refresh()
@@ -49,116 +41,35 @@ func _refresh() -> void:
 
 	var phase := workout_provider.get_phase(0)
 
-	if not phase:
-		return
+	if phase and ui_updater:
+		# UI updater might be null here
+		ui_updater.inject_phase(phase)
 
-	if rep_counter:
-		rep_counter.max_count = phase.reps
-
-	if flasher:
-		flasher.wait_time_seconds = phase.rep_duration_seconds
-
-	if pause_countdown:
-		pause_countdown.duration_seconds = int(phase.pause_duration_seconds)
-
-func do_countdown() -> void:
-	if flasher:
-		flasher.hide()
-
-		print("do_countdown: hid flasher")
-
-	if countdown:
-		countdown.show()
-		countdown.start_stop()
-
-		print("do_countdown: started countdown")
-
-	if status_message:
-		status_message.show()
-		status_message.message = StatusMessage.MessageType.GET_READY
-
+func to_countdown() -> void:
 	_current_phase = 0
 
 	var phase := workout_provider.get_phase(_current_phase)
 	_sets_remaining = phase.sets
 
-func move_to_flasher():
-	if countdown:
-		countdown.hide()
-		countdown.reset()
+	ui_updater.to_countdown()
 
-		print("move_to_flasher: reset countdown")
-
-	if pause_countdown:
-		pause_countdown.hide()
-		pause_countdown.reset()
-
-		print("move_to_flasher: reset pause_countdown")
-
+func to_in_progress() -> void:
 	var phase := workout_provider.get_phase(_current_phase)
-
 	_reps_remaining = phase.reps
 
-	if flasher:
-		flasher.show()
-		flasher.start_stop()
+	ui_updater.to_in_progress()
 
-		print("move_to_flasher: start_stopped flasher")
-
-	if status_message:
-		status_message.hide()
-
-	if rep_counter:
-		rep_counter.show()
-		rep_counter.stop()
-
-	if phase_counter:
-		phase_counter.inc()
-
-func pause() -> void:
-	if flasher:
-		flasher.hide()
-		flasher.stop()
-
-		print("Stopped flasher")
-
-	if status_message:
-		status_message.show()
-		status_message.message = StatusMessage.MessageType.PAUSING
-
+func to_pausing() -> void:
 	var phase := workout_provider.get_phase(_current_phase)
 
 	print("Pausing for %d second(s)" % phase.pause_duration_seconds)
 
-	if rep_counter:
-		rep_counter.hide()
-		rep_counter.stop()
+	ui_updater.to_pausing(phase.pause_duration_seconds)
 
-	if pause_countdown:
-		pause_countdown.duration_seconds = int(phase.pause_duration_seconds)
-		pause_countdown.show()
-		pause_countdown.start_stop()
+func to_ready() -> void:
+	print("Stopping")
 
-	if phase_counter:
-		phase_counter.complete()
-
-func stop() -> void:
-	if flasher:
-		flasher.hide()
-		flasher.stop()
-
-		print("Stopped flasher")
-
-	if status_message:
-		status_message.show()
-		status_message.message = StatusMessage.MessageType.PRESS_START
-
-	if rep_counter:
-		rep_counter.hide()
-		rep_counter.stop()
-
-	if phase_counter:
-		phase_counter.stop()
+	ui_updater.to_ready()
 
 func _on_flasher_flashed() -> void:
 	if _reps_remaining <= 0:
@@ -170,9 +81,7 @@ func _on_flasher_flashed() -> void:
 			_current_phase += 1
 
 			if _current_phase >= workout_provider.get_phase_count():
-				print("Stopping")
-
-				stop()
+				to_ready()
 			else:
 				print("%d set(s) remaining" % _sets_remaining)
 
@@ -180,32 +89,24 @@ func _on_flasher_flashed() -> void:
 
 				_sets_remaining = next_phase.sets
 
-				if rep_counter:
-					rep_counter.max_count = next_phase.reps
+				ui_updater.inject_phase(next_phase)
 
-				if flasher:
-					flasher.wait_time_seconds = next_phase.rep_duration_seconds
-
-				if pause_countdown:
-					pause_countdown.duration_seconds = int(next_phase.pause_duration_seconds)
-
-				pause()
+				to_pausing()
 		else:
 			print("%d set(s) remaining" % _sets_remaining)
 
-			pause()
+			to_pausing()
 	else:
 		_reps_remaining -= 1
 
 		print("%d reps remaining" % _reps_remaining)
 
-		if rep_counter:
-			rep_counter.inc()
+		ui_updater.add_rep()
 
 func _on_countdown_finished() -> void:
 	print("Countdown finished, moving to flasher")
 
-	move_to_flasher()
+	to_in_progress()
 
 func _on_countdown_cancelled() -> void:
 	print("Countdown cancelled")
@@ -214,8 +115,8 @@ func _on_key_listener_pressed_start() -> void:
 	if start_immediately:
 		print("Starting immediately")
 
-		move_to_flasher()
+		to_in_progress()
 	else:
 		print("Doing countdown before starting")
 
-		do_countdown()
+		to_countdown()
---
+++ scripts/ui_updater.gd
@@ -0,0 +1,120 @@
+@tool
+class_name UIUpdater extends Node
+
+@export
+var phase_counter: PhaseCounter
+
+@export
+var status_message: StatusMessage
+
+@export
+var rep_counter: RepCounter
+
+@export
+var countdown: Countdown
+
+@export
+var pause_countdown: Countdown
+
+@export
+var flasher: Flasher
+
+func inject_phase(phase: WorkoutPhase) -> void:
+	if rep_counter:
+		rep_counter.max_count = phase.reps
+
+	if flasher:
+		flasher.wait_time_seconds = phase.rep_duration_seconds
+
+	if pause_countdown:
+		pause_countdown.duration_seconds = int(phase.pause_duration_seconds)
+
+func add_rep() -> void:
+	if rep_counter:
+		rep_counter.inc()
+
+func to_countdown() -> void:
+	if flasher:
+		flasher.hide()
+
+		print("to_countdown: hid flasher")
+
+	if countdown:
+		countdown.show()
+		countdown.start_stop()
+
+		print("to_countdown: started countdown")
+
+	if status_message:
+		status_message.show()
+		status_message.message = StatusMessage.MessageType.GET_READY
+
+func to_in_progress() -> void:
+	if countdown:
+		countdown.hide()
+		countdown.reset()
+
+		print("to_in_progress: reset countdown")
+
+	if pause_countdown:
+		pause_countdown.hide()
+		pause_countdown.reset()
+
+		print("to_in_progress: reset pause_countdown")
+
+	if flasher:
+		flasher.show()
+		flasher.start_stop()
+
+		print("to_in_progress: start_stopped flasher")
+
+	if status_message:
+		status_message.hide()
+
+	if rep_counter:
+		rep_counter.show()
+		rep_counter.stop()
+
+	if phase_counter:
+		phase_counter.inc()
+
+func to_pausing(duration_seconds: float) -> void:
+	if flasher:
+		flasher.hide()
+		flasher.stop()
+
+		print("to_pausing: stopped flasher")
+
+	if status_message:
+		status_message.show()
+		status_message.message = StatusMessage.MessageType.PAUSING
+
+	if rep_counter:
+		rep_counter.hide()
+		rep_counter.stop()
+
+	if pause_countdown:
+		pause_countdown.duration_seconds = int(duration_seconds)
+		pause_countdown.show()
+		pause_countdown.start_stop()
+
+	if phase_counter:
+		phase_counter.complete()
+
+func to_ready() -> void:
+	if flasher:
+		flasher.hide()
+		flasher.stop()
+
+		print("to_ready: stopped flasher")
+
+	if status_message:
+		status_message.show()
+		status_message.message = StatusMessage.MessageType.PRESS_START
+
+	if rep_counter:
+		rep_counter.hide()
+		rep_counter.stop()
+
+	if phase_counter:
+		phase_counter.stop()

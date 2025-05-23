23b8796574cd4ad3a5a7357f08baebf352ba1199
Julian L <julian.lawrance@gmail.com>
2025-05-23 22:32:50 +0100

    Create different progress parameters (alpha/width) for flasher scene progress

--- scripts/flasher.gd
+++ scripts/flasher.gd
@@ -3,6 +3,8 @@ class_name Flasher extends ColorRect
 
 enum ProgressCurve { LINEAR_OUT, LINEAR_IN_OUT }
 
+enum ProgressParam { ALPHA, WIDTH }
+
 ## The time between flashes.
 @export_range(0.1, 5.0)
 var wait_time_seconds := 1.0
@@ -11,6 +13,13 @@ var wait_time_seconds := 1.0
 var progress_curve := ProgressCurve.LINEAR_OUT:
 	set(value):
 		progress_curve = value
+
+		_refresh()
+
+@export
+var progress_param := ProgressParam.ALPHA:
+	set(value):
+		progress_param = value
 
 		_refresh()
 
@@ -39,7 +48,14 @@ var curves: Dictionary[ProgressCurve, Curve] = {}:
 
 		_refresh()
 
+var _base_width := 0.0
+
 signal flashed
+
+func _ready() -> void:
+	_base_width = size.x
+
+	_refresh()
 
 func _process(delta: float) -> void:
 	if not started:
@@ -52,7 +68,20 @@ func _process(delta: float) -> void:
 		flashed.emit()
 
 func _refresh() -> void:
-	color.a = curves[progress_curve].sample(progress)
+	color.a = _compute_alpha()
+	size.x = _compute_width()
+
+func _compute_alpha() -> float:
+	if progress_param == ProgressParam.ALPHA:
+		return curves[progress_curve].sample(progress)
+
+	return 1.0
+
+func _compute_width() -> float:
+	if progress_param == ProgressParam.WIDTH:
+		return _base_width * curves[progress_curve].sample(progress)
+
+	return _base_width
 
 func start_stop() -> void:
 	if not started:
@@ -63,3 +92,7 @@ func start_stop() -> void:
 
 func stop() -> void:
 	started = false
+
+func _on_resized() -> void:
+	if not started:
+		_base_width = size.x

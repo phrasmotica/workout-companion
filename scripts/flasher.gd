a1e7d517864c7ba37ebda90ef0ea252f9a61b039
Julian L <julian.lawrance@gmail.com>
2025-05-24 12:12:55 +0100

    Re-implement flasher scene tree (ensuring it works in main scene) and add height as a progress param value

--- scripts/flasher.gd
+++ scripts/flasher.gd
@@ -1,9 +1,9 @@
 @tool
-class_name Flasher extends ColorRect
+class_name Flasher extends HBoxContainer
 
 enum ProgressCurve { LINEAR_OUT, LINEAR_IN_OUT }
 
-enum ProgressParam { ALPHA, WIDTH }
+enum ProgressParam { ALPHA, WIDTH, HEIGHT }
 
 ## The time between flashes.
 @export_range(0.1, 5.0)
@@ -48,6 +48,21 @@ var curves: Dictionary[ProgressCurve, Curve] = {}:
 
 		_refresh()
 
+@onready
+var width_container: VBoxContainer = %WidthContainer
+
+@onready
+var width_spacer: Control = %WidthSpacer
+
+@onready
+var height_container: VBoxContainer = %HeightContainer
+
+@onready
+var height_spacer: Control = %HeightSpacer
+
+@onready
+var color_rect: ColorRect = %ColorRect
+
 var _base_width := 0.0
 
 signal flashed
@@ -68,20 +83,48 @@ func _process(delta: float) -> void:
 		flashed.emit()
 
 func _refresh() -> void:
-	color.a = _compute_alpha()
-	size.x = _compute_width()
+	if color_rect:
+		color_rect.color.a = _compute_alpha()
+
+	var width_ratio := _compute_width_ratio()
+
+	if width_container:
+		width_container.size_flags_stretch_ratio = width_ratio
+
+	if width_spacer:
+		width_spacer.size_flags_stretch_ratio = 1.0 - width_ratio
+
+	var height_ratio := _compute_height_ratio()
+
+	if height_container:
+		height_container.size_flags_stretch_ratio = height_ratio
+
+	if height_spacer:
+		height_spacer.size_flags_stretch_ratio = 1.0 - height_ratio
 
 func _compute_alpha() -> float:
 	if progress_param == ProgressParam.ALPHA:
-		return curves[progress_curve].sample(progress)
+		return _sample_curve()
 
 	return 1.0
 
-func _compute_width() -> float:
+func _compute_width_ratio() -> float:
 	if progress_param == ProgressParam.WIDTH:
-		return _base_width * curves[progress_curve].sample(progress)
+		return _sample_curve()
 
-	return _base_width
+	return 1.0
+
+func _compute_height_ratio() -> float:
+	if progress_param == ProgressParam.HEIGHT:
+		return _sample_curve()
+
+	return 1.0
+
+func _sample_curve() -> float:
+	if curves.has(progress_curve):
+		return curves[progress_curve].sample(progress)
+
+	return 1.0
 
 func start_stop() -> void:
 	if not started:
